3f8a5b4c967032c07152d2e58858e4c2a52a37ed
Twiitchter <andylutze6090@gmail.com>
2021-07-18 20:20:37 +1000

    First

---
+++ nui/banking.js
@@ -0,0 +1,62 @@
+/*
+
+*/
+
+function Populate(data) {
+
+};
+
+function Deposit(amount) {
+    $.post('https://c.banking/Client:Banking:Deposit', JSON.stringify({
+        data: {},
+    }));
+};
+
+function Wtchdrawl(amount) {
+    $.post('https://c.banking/Client:Banking:Withdrawl', JSON.stringify({
+        data: {},
+    }));
+};
+
+function Transfer(amount, too) {
+    $.post('https://c.banking/Client:Banking:Transfer', JSON.stringify({
+        data: {},
+    }));
+};
+
+function PayBill(amount, bill) {
+    $.post('https://c.banking/Client:Banking:PayBill', JSON.stringify({
+        data: {},
+    }));
+};
+
+function PayLoan(amount) {
+    $.post('https://c.banking/Client:Banking:PayLoan', JSON.stringify({
+        data: {},
+    }));
+};
+
+$(document).ready(function () {
+    window.onload = (e) => {
+        window.addEventListener('message', (event) => {
+            let data = event.data;
+            switch (data.message) {
+                case 'OnOpen':
+                    Populate(data.Packet)
+                    break;
+                case 'OnUpdate':
+                    Populate(data.Packet)
+                    break;
+                case 'default':
+                    break;
+            }
+        });
+    };
+    document.onkeyup = function(data){
+        if (data == 27){
+            $.post('http://c.banking/Client:NUI:OnClose', JSON.stringify({
+
+            }));
+        }
+    };    
+});
